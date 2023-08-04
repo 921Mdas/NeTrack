18849a0ea80b33a4b7739385b54573367aed4747
Deo G <66809588+921Mdas@users.noreply.github.com>
2023-08-04 09:52:42 -0400

    production changing urls to match

--- svelte/src/api.js
+++ svelte/src/api.js
@@ -1,94 +1,92 @@
 /** @type {import('./$types').PageLoad} */
 import { updateNetStore, updateGraphStore, findNetCaseToEdit, isEditing } from './store';
+const devURL = 'http://localhost:3000/';
+const prodURL = 'https://csto.onrender.com/';
+
 export const load = async () => {
-    try {
-        const response = await fetch('http://localhost:3000/net');
-        const { netOptions } = await response.json();
-        return netOptions;
-    }
-    catch (error) {
-        console.log('something went wrong 🛑');
-        console.log(error);
-    }
-    return null;
+	try {
+		const response = await fetch(`${prodURL}/net`);
+		const { netOptions } = await response.json();
+		return netOptions;
+	} catch (error) {
+		console.log('something went wrong 🛑');
+		console.log(error);
+	}
+	return null;
 };
 export const getReps = async () => {
-    try {
-        const response = await fetch('http://localhost:3000/rep');
-        const data = await response.json();
-        return data;
-    }
-    catch (error) {
-        console.log('something went wrong 🛑');
-        console.log(error);
-    }
+	try {
+		const response = await fetch(`${prodURL}/rep`);
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.log('something went wrong 🛑');
+		console.log(error);
+	}
 };
 export const addNetCasesToStore = async (input) => {
-    try {
-        await fetch('http://localhost:3000/net', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                input
-            })
-        });
-        updateNetStore();
-    }
-    catch (error) {
-        console.log('something went wrong 🛑');
-        console.log(error);
-    }
+	try {
+		await fetch(`${prodURL}/net`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				input
+			})
+		});
+		updateNetStore();
+	} catch (error) {
+		console.log('something went wrong 🛑');
+		console.log(error);
+	}
 };
 export const deleteNetCase = async (id) => {
-    try {
-        await fetch(`http://localhost:3000/deletenet/${id}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        updateNetStore();
-        updateGraphStore();
-        isEditing.update((prev) => {
-            prev = false;
-            return prev;
-        });
-    }
-    catch (error) {
-        console.log('something went wrong 🛑');
-        console.log(error);
-    }
+	try {
+		await fetch(`${prodURL}/deletenet/${id}`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		updateNetStore();
+		updateGraphStore();
+		isEditing.update((prev) => {
+			prev = false;
+			return prev;
+		});
+	} catch (error) {
+		console.log('something went wrong 🛑');
+		console.log(error);
+	}
 };
 export const editNet = async (id) => {
-    isEditing.update((bool) => {
-        bool = true;
-        return bool;
-    });
-    // received the new version data and use the id to send both to backend
-    findNetCaseToEdit(id);
+	isEditing.update((bool) => {
+		bool = true;
+		return bool;
+	});
+	// received the new version data and use the id to send both to backend
+	findNetCaseToEdit(id);
 };
 export const addEditedNetCaseToStore = async (formData) => {
-    try {
-        await fetch(`http://localhost:3000/editnet/${formData._id}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                formData
-            })
-        });
-        updateNetStore();
-        updateGraphStore();
-        isEditing.update((bool) => {
-            bool = false;
-            return bool;
-        });
-    }
-    catch (error) {
-        console.log('something went wrong 🛑');
-        console.log(error);
-    }
+	try {
+		await fetch(`${prodURL}/editnet/${formData._id}`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				formData
+			})
+		});
+		updateNetStore();
+		updateGraphStore();
+		isEditing.update((bool) => {
+			bool = false;
+			return bool;
+		});
+	} catch (error) {
+		console.log('something went wrong 🛑');
+		console.log(error);
+	}
 };
